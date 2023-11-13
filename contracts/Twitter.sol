d5b3bc5b55c6a09d2fed9f63ec83e1b6bb047769
Marwan Mai <emef4z@gmail.com>
2023-11-13 16:30:56 +0100

    Code cleanup : Clean createTweet function

--- contracts/Twitter.sol
+++ contracts/Twitter.sol
@@ -12,33 +12,9 @@ contract Twitter {
   Tweet[] private _tweets;
 
   mapping(address => uint[]) private _tweetsByAuthor;
-  uint myNumber = 3;
-  string myString = 'Hello';
-  bool myBool = true;
-
-  // function createTweet(string memory _content) external {
-  //   uint _id = tweets.length;
-  //   uint _timestamp = block.timestamp;
-  //   Tweet memory _tweet = Tweet(_id, msg.sender, _content, _timestamp);
 
   function createTweet(string memory _content) external {
-    _tweets.puh(Tweet(_tweets.length, msg.sender, _content, block.timestamp));
+    _tweets.push(Tweet(_tweets.length, msg.sender, _content, block.timestamp));
     _tweetsByAuthor[msg.sender].push(_tweets.length);
-    
-    // _tweets.push(_tweet);
-    // _tweetByAuthor[msg.sender].push(_id)
   }
-
-  // address myAdress = 0x5B38;
-
-  // function operateNumber() public pure returns (uint) {
-  //   uint a = 1;
-  //   uint b = 2;
-  //   uint c = a + b;
-
-    return c;
-  }
-  // constructor() {
-
-  // }
 }
