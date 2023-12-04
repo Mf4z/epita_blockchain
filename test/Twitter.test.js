07ce1f2225e13a5d42350f8078b9e8b123992b9f
Marwan Mai <emef4z@gmail.com>
2023-12-04 15:21:42 +0100

    Add editTweet test case

--- test/Twitter.test.js
+++ test/Twitter.test.js
@@ -13,10 +13,23 @@ describe.only('Twitter', () => {
   it('should get all the tweets', async () => {
     const tweets = await twitter.getTweets();
     expect(tweets.length).to.equal(0);
+  });
 
+  it('should create a tweet', async () => {
     await twitter.createTweet('Hello, world!');
-    const tweets2 = await twitter.getTweets();
-    expect(tweets2.length).to.equal(1);
+    const tweets = await twitter.getTweets();
+
+    expect(tweets.length).to.equal(1);
+    expect(tweets[0].content).to.equal('Hello,world!');
+  });
+
+  it("should be able to edit one's own tweet", async () => {
+    await twitter.createTweet('Hello, world!');
+    await twitter.editTweet(0, 'Goodbye, world!');
+    const tweets = await twitter.getTweets();
+
+    expect(tweets.length).to.equal(1);
+    expect(tweets[0].content).to.equal('Goodbye,world!');
   });
 
   // it('should be erased', () => {});
